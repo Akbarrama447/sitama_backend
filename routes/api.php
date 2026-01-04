cec7d67ad41ba768299f6f66ee731fbba1ae6be8
Akbarrama477 <akbarrama3061@gmail.com>
2026-01-04 16:22:50 +0700

    resolve conflict merge 2

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,7 @@ use App\Http\Controllers\Api\DokumenSidangController;
 use App\Http\Controllers\Api\FileDokumenSidangController;
 use App\Http\Controllers\Api\SyaratSidangController;
 use App\Http\Controllers\Api\RevisiTugasAkhirController;
-<<<<<<< HEAD
 use App\Http\Controllers\TestApiController;
-=======
->>>>>>> 603c094c23a34b5a7f84dcfd14cdf2d885d6fe2c
 
 /*
 |--------------------------------------------------------------------------
@@ -76,8 +73,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/revisi-tugas-akhir-by-ta/{tugas_akhir_id}', [RevisiTugasAkhirController::class, 'getByTugasAkhir']);
     Route::post('/revisi-tugas-akhir-untuk-saya', [RevisiTugasAkhirController::class, 'storeForCurrentUser']);
     Route::get('/revisi-tugas-akhir-saya', [RevisiTugasAkhirController::class, 'getForCurrentUser']);
-<<<<<<< HEAD
 
-=======
->>>>>>> 603c094c23a34b5a7f84dcfd14cdf2d885d6fe2c
 });
